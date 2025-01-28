bf9e7e159b6a46063469b06a32eefe68409201cf
Tim Irmler <zwazel@hotmail.com>
2025-01-28 23:28:24 +0100

    first version of message data

--- src/main/java/dev/zwazel/messages/MessageParser.java
+++ src/main/java/dev/zwazel/messages/MessageParser.java
@@ -2,8 +2,18 @@ package dev.zwazel.messages;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.zwazel.messages.model.GameStateUpdateData;
 
 public class MessageParser {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static Object parseMessageData(NetworkMessageType messageType, JsonNode messageData) throws Exception {
+        return switch (messageType) {
+            case GAME_UPDATE -> mapper.treeToValue(messageData, GameStateUpdateData.class);
+            default -> throw new IllegalArgumentException("Unknown message type: " + messageType);
+        };
+    }
+
     public static MessageContainer parseMessage(byte[] data) throws Exception {
         // Parse JSON directly
         ObjectMapper mapper = new ObjectMapper();
@@ -14,6 +24,8 @@ public class MessageParser {
         message.setTarget(MessageTarget.fromOrdinal(root.get("target").asInt()));
         message.setMessageType(NetworkMessageType.fromOrdinal(root.get("message_type").asInt()));
         message.setMessageData(root.get("message_data"));
+        Object specificData = parseMessageData(message.getMessageType(), message.getMessageData());
+        System.out.println("Deserialized specific message data: " + specificData);
 
         return message;
     }
---
+++ src/main/java/dev/zwazel/messages/model/GameStateUpdateData.java
@@ -0,0 +1,8 @@
+package dev.zwazel.messages.model;
+
+import lombok.Data;
+
+@Data
+public class GameStateUpdateData {
+    private int tick;
+}
