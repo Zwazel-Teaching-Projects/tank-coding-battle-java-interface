79a15c988d5a2c5e4e7f5bb077c816d0c5e8d350
Tim Irmler <zwazel@hotmail.com>
2025-02-09 18:52:32 +0100

    print errors + lobby already running error

--- src/main/java/dev/zwazel/internal/messages/data/MessageError.java
+++ src/main/java/dev/zwazel/internal/messages/data/MessageError.java
@@ -1,6 +1,7 @@
 package dev.zwazel.internal.messages.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.zwazel.internal.InternalGameWorld;
 import dev.zwazel.internal.messages.MessageData;
 
 public record MessageError(@JsonProperty("error_type") ErrorTypes error,
@@ -8,5 +9,11 @@ public record MessageError(@JsonProperty("error_type") ErrorTypes error,
     enum ErrorTypes {
         INVALID_TARGET,
         LOBBY_MANAGEMENT_ERROR,
+        LOBBY_ALREADY_RUNNING,
+    }
+
+    @Override
+    public void applyOnReceive(InternalGameWorld internalWorld) {
+        System.err.println("Error: " + error + " - " + errorMessage);
     }
 }
