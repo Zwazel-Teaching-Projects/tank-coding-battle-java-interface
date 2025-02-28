7ca44c7c7d07c14b2f7d5384aa418bb4ddca8046
Tim Irmler <zwazel@hotmail.com>
2025-02-28 17:40:23 +0100

    improved api

--- src/main/java/dev/zwazel/internal/game/state/ClientState.java
+++ src/main/java/dev/zwazel/internal/game/state/ClientState.java
@@ -1,5 +1,7 @@
 package dev.zwazel.internal.game.state;
 
+import dev.zwazel.internal.PublicGameWorld;
+import dev.zwazel.internal.connection.client.ConnectedClientConfig;
 import dev.zwazel.internal.game.transform.Transform;
 
 /**
@@ -10,9 +12,14 @@ import dev.zwazel.internal.game.transform.Transform;
  * @param transformTurret The transform of the turret of the tank (relative to the body)
  * @param state           The state of the player (e.g. alive, dead)
  * @param shootCooldown   The amount of ticks until the tank can shoot again
+ * @param currentHealth   The current health of the tank
  */
 public record ClientState(long id, Transform transformBody, Transform transformTurret, PlayerState state,
                           long shootCooldown, float currentHealth) {
+    public ConnectedClientConfig getConnectedClientConfig(PublicGameWorld world) {
+        return world.getConnectedClientConfig(this).orElse(null);
+    }
+
     public enum PlayerState {
         ALIVE,
         DEAD
