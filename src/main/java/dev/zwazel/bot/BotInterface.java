5a44ca88796f876505250dbda56191306da03fce
Tim Irmler <zwazel@hotmail.com>
2025-02-28 17:31:08 +0100

    javadoc stuff

--- src/main/java/dev/zwazel/bot/BotInterface.java
+++ src/main/java/dev/zwazel/bot/BotInterface.java
@@ -2,13 +2,21 @@ package dev.zwazel.bot;
 
 import dev.zwazel.internal.PublicGameWorld;
 
-/*
- TODO: This is the interface that the students will implement. they should not implement/extend the specific tanks
-  instead they should implement this interface and the game will provide them with the implemented bot interface
-  that will be used to interact with the game world.
- */
 public interface BotInterface {
+    /**
+     * Set up the bot.
+     * Gets called once at the start of the game.
+     *
+     * @param world The game world.
+     */
     void setup(PublicGameWorld world);
 
+    /**
+     * Process a tick.
+     * Gets called every tick.
+     * Should be used to schedule commands, like moving or shooting.
+     *
+     * @param world The game world.
+     */
     void processTick(PublicGameWorld world);
 }
