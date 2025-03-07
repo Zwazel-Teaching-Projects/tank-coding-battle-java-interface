29b7d1bead0fac9676ee049bc4b4126a5e6307ba
Tim Irmler <zwazel@hotmail.com>
2025-03-07 17:28:14 +0100

    updated received flag game state

--- src/main/java/dev/zwazel/internal/game/state/FlagGameState.java
+++ src/main/java/dev/zwazel/internal/game/state/FlagGameState.java
@@ -2,6 +2,8 @@ package dev.zwazel.internal.game.state;
 
 import dev.zwazel.internal.game.state.flag.FlagState;
 import dev.zwazel.internal.game.transform.Transform;
+import dev.zwazel.internal.game.transform.Vec3;
 
-public record FlagGameState(long flagId, String team, Transform transform, FlagState state) {
+public record FlagGameState(long flagId, long flagNumber, Vec3 colliderSize, String team, Transform transform,
+                            FlagState state) {
 }
