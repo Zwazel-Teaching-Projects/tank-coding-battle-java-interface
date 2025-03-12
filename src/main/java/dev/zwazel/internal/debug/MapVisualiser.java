3b237313de6d5a3765599ee24ae2f8ea38ec737d
Tim Irmler <zwazel@hotmail.com>
2025-03-12 19:34:28 +0100

    drawing a path :)
    pathfinding (very basic) works :)
    no climbing so far!

--- src/main/java/dev/zwazel/internal/debug/MapVisualiser.java
+++ src/main/java/dev/zwazel/internal/debug/MapVisualiser.java
@@ -1,18 +1,24 @@
 package dev.zwazel.internal.debug;
 
-import dev.zwazel.internal.InternalGameWorld;
 import dev.zwazel.internal.PublicGameWorld;
 import dev.zwazel.internal.game.map.MapDefinition;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-@RequiredArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class MapVisualiser extends JPanel {
     private final PublicGameWorld world;
-
     private final int CELL_SIZE = 50;
+    private DrawingMode drawingMode = DrawingMode.HEIGHT;
+    /**
+     * A list of points to draw on the map. Used for drawing paths.
+     */
+    private ArrayList<Point> path = new ArrayList<>();
 
     public void showMap() {
         int width = ((int) world.getGameConfig().mapDefinition().width() + 1) * CELL_SIZE;
@@ -33,6 +39,14 @@ public class MapVisualiser extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         MapDefinition mapDefinition = world.getGameConfig().mapDefinition();
 
+        // Switch drawing mode
+        switch (drawingMode) {
+            case HEIGHT -> drawHeightMap(g2d, mapDefinition);
+            case PATH -> drawPath(g2d, mapDefinition);
+        }
+    }
+
+    private void drawHeightMap(Graphics2D g2d, MapDefinition mapDefinition) {
         // Determine min and max heights to normalize cell colors
         float min = Float.MAX_VALUE;
         float max = Float.MIN_VALUE;
@@ -62,6 +76,33 @@ public class MapVisualiser extends JPanel {
                         CELL_SIZE,
                         CELL_SIZE
                 );
+            }
+        }
+
+        drawCellBorders(g2d, mapDefinition);
+    }
+
+    private void drawPath(Graphics2D g2d, MapDefinition mapDefinition) {
+        // draw path, path is a list of points representing cells in the grid. Each point is a cell in the grid.
+        // Draw line between each point in the path
+        g2d.setColor(Color.RED);
+        for (int i = 0; i < path.size() - 1; i++) {
+            Point p1 = path.get(i);
+            Point p2 = path.get(i + 1);
+            g2d.drawLine(
+                    p1.x * CELL_SIZE + CELL_SIZE / 2,
+                    p1.y * CELL_SIZE + CELL_SIZE / 2,
+                    p2.x * CELL_SIZE + CELL_SIZE / 2,
+                    p2.y * CELL_SIZE + CELL_SIZE / 2
+            );
+        }
+
+        drawCellBorders(g2d, mapDefinition);
+    }
+
+    private void drawCellBorders(Graphics2D g2d, MapDefinition mapDefinition) {
+        for (int x = 0; x < mapDefinition.width(); x++) {
+            for (int y = 0; y < mapDefinition.depth(); y++) {
                 // Draw cell border
                 g2d.setColor(Color.BLACK);
                 g2d.drawRect(
@@ -73,11 +114,17 @@ public class MapVisualiser extends JPanel {
                 // Draw the height value in the cell
                 g2d.setColor(Color.BLACK);
                 g2d.drawString(
-                        String.format("%.2f", tileHeight),
+                        String.format("%.2f", mapDefinition.tiles()[y][x]),
                         x * CELL_SIZE + 5,
                         y * CELL_SIZE + 15
                 );
             }
         }
+    }
+
+    // Enum for switching drawing modes
+    public enum DrawingMode {
+        HEIGHT,
+        PATH
     }
 }
